49a1a7d522538d913ab2b948f05a116351818cd6
Mohamed aziz <benhmida.mohamedaziz@esprit.tn>
2023-06-15 04:05:26 -0700

    comùmit 54

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,5 @@
-/*
- *starting of the programe 
- *we start with the include , here we included the stdio.h 
- */
 #include <stdio.h>
-/*
- *and here we start with the main which is our main program
- *our programe will print Programming is like building a multilingual puzzle
- *the return here is 0 
- */
+
 
 int main(void)
 {
