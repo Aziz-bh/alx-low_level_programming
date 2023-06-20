a40b2cce2f62301f37f65657939ca512dba74e39
Mohamed aziz <benhmida.mohamedaziz@esprit.tn>
2023-06-20 04:16:23 -0700

    commit fixes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,13 +4,13 @@
 */
 void print_alphabet_x10(void)
 {
-    int i;
-    char la;
+int i;
+char la;
 
-    for (i=0;i<= 9;i++)
+for (i = 0; i <= 9; i++)
       {
-	 for(la ='a';la<='z';la++)
-          _putchar (la);
+	 for(la = 'a'; la <= 'z'; la++)
+		_putchar(la);
         _putchar('\n');
       }
 }
