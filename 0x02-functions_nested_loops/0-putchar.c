b0ce6effce72eedaf172d33de35c24bc5d327221
Mohamed aziz <benhmida.mohamedaziz@esprit.tn>
2023-06-20 02:44:07 -0700

    commit 1'

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,17 @@
-#include "main.h"
-/**
- * main - prints the .
- * Return: 0.
+/* 
+* File: 0-putchar.c 
+ *
  */
-int main(void)
-{
+
+ #include "main.h" 
+
+ /** 
+ * main-print putchar followed by a new line
+ * 
+ *Return :always 0
+ */
+  int main(void) 
+ {
 	printf("_putchar\n");
 
 	return(0);
