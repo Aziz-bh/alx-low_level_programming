56d3391e850ceb66bb16a73cadf306522b711dea
Mohamed aziz <benhmida.mohamedaziz@esprit.tn>
2023-06-20 02:45:02 -0700

    commit fixes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,6 @@
-/* 
-* File: 0-putchar.c 
+
+ /*
+ * File: 0-putchar.c 
  *
  */
 
@@ -12,7 +13,15 @@
  */
   int main(void) 
  {
-	printf("_putchar\n");
-
-	return(0);
-}
+      _putchar('_' );
+      _putchar ('p' );
+      _putchar ('u' );
+      _putchar ('t' );
+      _putchar ('c' );
+      _putchar ('h' );
+      _putchar ('a' );
+      _putchar ('r' );
+      _putchar ('\n');
+      
+       return (0) ;
+} 
