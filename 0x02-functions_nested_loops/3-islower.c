9edcaafa8371086e4d814eb0566acdb268702294
Mohamed aziz <benhmida.mohamedaziz@esprit.tn>
2023-06-20 04:22:45 -0700

    commit 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - check if a character is lowercase
+ * @c: the character 
+ *
+ * Return: 1 if the lowercase is found, 0 otherwise
+ */
+int _islower(int c)
+{  
+    if (c >= 'a' && c <= 'z')
+        return (1);
+    else
+        return (0);
+}
+ 
