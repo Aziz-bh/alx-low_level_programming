d816443b236e0dfcd82e07a58a4d60c3065f58ef
root <benhmida.mohamedaziz@esprit.tn>
2023-07-13 15:02:39 -0700

    ommit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,23 +1,18 @@
-/*
- * File: 0-malloc_checked.c
- * Auth: Aziz ben hmida
- */
-
 #include "main.h"
 #include <stdlib.h>
 
 /**
- * malloc_checked - Allocates memory using malloc.
- * @b: The number of bytes to be allocated.
- *
- * Return: A pointer to the allocated memory.
+ * malloc_checked - allocates memory using malloc
+ * @b: int size of memory to be allocated
+ * Return: pointer to allocated memory
  */
+
 void *malloc_checked(unsigned int b)
 {
-	void *mem = malloc(b);
+	void *ptr;
 
-	if (mem == NULL)
+	ptr = malloc(b);
+	if (ptr == NULL)
 		exit(98);
-
-	return (mem);
+	return (ptr);
 }
